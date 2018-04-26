83abacbf132f7d4cba75d8d8d3b522e45cd65ecd
Jmo5896 <jmo5896@moore-clan.net>
2018-04-26 07:40:09 -0600

    3rd commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,53 +6,68 @@ var guessesLeft = 9;
 var listOfGuesses = [];
 
 
-document.onkeydown = function () {
+document.onkeyup = function () {
     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-      
-    if ((userGuess === "a") || (userGuess === "b") || (userGuess === "c") || (userGuess === "d") || (userGuess === "e") || (userGuess === "f") || (userGuess === "g") || (userGuess === "h") || (userGuess === "i") || (userGuess === "j") || (userGuess === "k") || (userGuess === "l") || (userGuess === "m") || (userGuess === "n") || (userGuess === "o") || (userGuess === "p") || (userGuess === "q") || (userGuess === "r") || (userGuess === "s") || (userGuess === "t") || (userGuess === "u") || (userGuess === "v") || (userGuess === "w") || (userGuess === "x") || (userGuess === "y") || (userGuess === "z")) {
-
-        if (userGuess === compGuess) {
-            wins++;
-            alert("You won sugar bear!!!  The letter was: " + compGuess);
-            //reset
-            guessesLeft = 9;
-            compGuess = compChoices[Math.floor(Math.random()*compChoices.length)];
-            listOfGuesses = [];
-        
-        } else if (guessesLeft === 0) {
-            losses++;
-            
-            //reset
-            guessesLeft = 9;
-            compGuess = compChoices[Math.floor(Math.random()*compChoices.length)];
-            listOfGuesses = [];
-        
+    var found = false; 
+    
+    // checks to see if the userGuess is inside the array listofGuesses, 
+    //if it is the program won't proceed
+    //the "break" command ends the loop
+    for( var y = 0; y < listOfGuesses.length; y++){
+        if (listOfGuesses[y] === userGuess) {
+            found = true;
+            break;
         } else {
-            guessesLeft--;
-
-            //push letter to listOfGuesses
-            listOfGuesses.push(userGuess);            
+            found = false;
         }
-        
-        // function consoleInside(arr) {
-        //     console.log("-------------");
-            
-        //     for ( var i = 0; i < arr.length; i++) {
-        //         console.log(arr[i]);
-        //     }
-            
-        //     console.log("-------------");
-        // }
-
-    // consoleInside(listOfGuesses);
-    // console.log(guessesLeft);
-    // console.log(userGuess);
-    // console.log(compGuess);
     }
+      if (found === false) {
+
+        if ((userGuess === "a") || (userGuess === "b") || (userGuess === "c") || (userGuess === "d") || (userGuess === "e") || (userGuess === "f") || (userGuess === "g") || (userGuess === "h") || (userGuess === "i") || (userGuess === "j") || (userGuess === "k") || (userGuess === "l") || (userGuess === "m") || (userGuess === "n") || (userGuess === "o") || (userGuess === "p") || (userGuess === "q") || (userGuess === "r") || (userGuess === "s") || (userGuess === "t") || (userGuess === "u") || (userGuess === "v") || (userGuess === "w") || (userGuess === "x") || (userGuess === "y") || (userGuess === "z")) {
+
+            if (userGuess === compGuess) {
+                wins++;
+                alert("You won!!!  The letter was: " + compGuess);
+                //reset
+                guessesLeft = 9;
+                compGuess = compChoices[Math.floor(Math.random()*compChoices.length)];
+                listOfGuesses = [];
+            
+            } else if (guessesLeft === 0) {
+                losses++;
+                
+                //reset
+                guessesLeft = 9;
+                compGuess = compChoices[Math.floor(Math.random()*compChoices.length)];
+                listOfGuesses = [];
+            
+            } else {
+                guessesLeft--;
+
+                //push letter to listOfGuesses
+                listOfGuesses.push(userGuess);            
+            }
+            
+            //this function takes the letters inside of listofGuesses and puts them in the html
+            function consoleInside(arr) {
+                
+                for ( var i = 0; i < arr.length; i++) {
+                    document.querySelector("#guesses").innerHTML ="Your Guesses so far: " + arr;
+                }
+                
+            }
+            consoleInside(listOfGuesses);
+            
+            // console.log(listOfGuesses);
+            // console.log(found);
+            // console.log(userGuess);
+            // console.log(compGuess);
+        }
+      }    
 
    var html =  "<p>Wins: " + wins + "</p>" +
                 "<p>Losses: " + losses + "</p>" +
-                "<p>Guesses Left: " + guessesLeft + "</p>" +
-                "<p>Your Guesses so far: coming soon" + "</p>";
+                "<p>Guesses Left: " + guessesLeft + "</p>"; 
+                
     document.querySelector("#game").innerHTML = html;
 }
